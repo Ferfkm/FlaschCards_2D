2b0da09efc3c1155e14a51a3003e06a0f3412ddd
Fernanda Fernandes <98698976+Ferfkm@users.noreply.github.com>
2024-10-29 11:49:57 +0000

    pergutnas js

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Programação',
+    'O que é python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'HTML é uma linguagem de marcação utilizada na construção de páginas na Web.'
+)
+criaCartao(
+    'Filosofia',
+    'O que é Renascimento?',
+    'O Renascimento foi um movimento cultural, artístico e político, surgido na Itália no século XIV.'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função ?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
